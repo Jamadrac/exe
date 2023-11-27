e829d98863586a70e88158d64b88ffc8c70376f8
jamadrac <rabbikindalo001@gmail.com>
2023-11-27 07:54:50 +0200

    sd

--- para.py
+++ para.py
@@ -12,28 +12,28 @@ class Calculator:
         if b != 0:
             return a / b
         else:
-            raise ValueError("Cannot divide by zero")
+            return "Cannot divide by zero"
 
 
 # Unit tests
 def test_calculator():
     calculator = Calculator()
 
-    # # Test addition
-    # assert calculator.add(2, 3) == 5
-    # assert calculator.add(-1, 1) == 0
+    # Test addition
+    assert calculator.add(2, 3) == 5
+    assert calculator.add(-1, 1) == 0
 
-    # # Test subtraction
-    # assert calculator.subtract(5, 3) == 2
-    # assert calculator.subtract(0, 0) == 0
+    # Test subtraction
+    assert calculator.subtract(5, 3) == 2
+    assert calculator.subtract(0, 0) == 0
 
-    # # Test multiplication
-    # assert calculator.multiply(2, 4) == 8
-    # assert calculator.multiply(-3, 2) == -6
+    # Test multiplication
+    assert calculator.multiply(2, 4) == 8
+    assert calculator.multiply(-3, 2) == -6
 
-    # # Test division
-    # assert calculator.divide(8, 2) == 4
-    # assert calculator.divide(5, 0)  # This should raise a ValueError
+    # Test division
+    assert calculator.divide(8, 2) == 4
+    assert calculator.divide(5, 0) == "Cannot divide by zero"
 
 
 if __name__ == "__main__":
